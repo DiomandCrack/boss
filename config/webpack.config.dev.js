8d6b9112117b65d54c3a2ad18e223383d5cb05a6
DiomandCrack <zk05161219@gmail.com>
2018-03-26 21:27:56 +0800

    add some dependencies

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -194,7 +194,7 @@ module.exports = {
               {loader:require('style-loader')}, 
               {loader:require('css-loader')}, 
               {loader:require('sass-loader')}],
-        }
+          },
           // "file" loader makes sure those assets get served by WebpackDevServer.
           // When you `import` an asset, you get its (virtual) filename.
           // In production, they would get copied to the `build` folder.
